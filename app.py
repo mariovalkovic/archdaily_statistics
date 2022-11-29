db443db51f6fcd01dea3046ceaf3c221fb9fb43d
Mario Valkovic <62554294+mariovalkovic@users.noreply.github.com>
2022-11-30 00:29:12 +0100

    .

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import pandas as pd
 import plotly.express as px
 import webbrowser
 
+# Page configuration
 st.set_page_config(page_title='Archdaily statistics')
 st.title('Archdaily statistics')
 
 col1, col2 = st.columns([1, 1])
 
+# Open csv data, get number of projects
 df = pd.read_csv('projects.csv')
 size = df.shape[0]
 
+# Define data to display
 metrics = col1.radio('Metrics to display', ['year', 'architect', 'country'])
 plot_type = col2.radio('Plot type', ['bar', 'pie'])
 
+# Plot type base on radio button input from user
 if plot_type == 'bar':
     fig = px.histogram(df, x = metrics, text_auto=True)
+    fig.update_traces(marker=dict(color='rgb(0, 150, 255)'))
     fig.update_layout(bargap=0.2)
     st.plotly_chart(fig)
 else:    
@@ -24,17 +30,15 @@ else:
     fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
     st.plotly_chart(fig)
 
-# table_type = st.radio('Table style', ['streamlit', 'clickable HTML'])
-# if table_type == 'streamlit': 
-#     st.dataframe(df)
-# else:
-#     st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
-    
+# Display data frame 
 st.dataframe(df)    
 st.success('Showing ' + str(size) + ' projects from dataset')
 
+# Redirect to project page on archdaily.com (for now only localhost)
 input = st.text_input('Project id from the table', 'paste id here')
 if st.button('Open project on ArchDaily'):
     webbrowser.open_new_tab('https://www.archdaily.com/' + str(input))
+
+# Download data as csv
 with open('projects.csv') as f:
     st.download_button(label = 'Download csv', data = df.to_csv(), mime = 'text/csv', file_name = 'results.csv')
--- scraper.py
+++ scraper.py
@@ -1,14 +1,17 @@
 import requests
 import pandas as pd
 
+# Defining API endpoint and known parameters
 endpoint = 'https://www.archdaily.com/search/api/v1/us/projects?page='
 page = '1'
 pages = 833
 
+# API request
 def main_request(endpoint, page):
     r = requests.get(endpoint + f'{page}')
     return r.json()
 
+# Saving selected parameters from json into a list of dictionaries
 def parse_json(response):
     dictionary = []
     for item in response['results']:
@@ -22,14 +25,13 @@ def parse_json(response):
         dictionary.append(dict_entry)
     return dictionary
 
+# Scraping
 mainlist = []
 data = main_request(endpoint, page)
 for page in range(1, pages+1):
     print(page)
     mainlist.extend(parse_json(main_request(endpoint, page)))
 
+# Pandas data frame and saving csv
 df = pd.DataFrame(mainlist)
 df.to_csv('projects_raw.csv', index=False)
-
-# print(len(mainlist))
-# print(df.head())
